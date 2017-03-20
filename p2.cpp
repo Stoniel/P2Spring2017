c2b579a7f649b357a2bdab8050a9cf6bc880219e
JEAN BAE <jean@nike.cs.uga.edu>
2017-03-20 17:38:49 -0400

    attempt to add menu

--- p2.cpp
+++ p2.cpp
@@ -19,9 +19,25 @@ int main(const int argc, const char * argv []){
   WINDOW *pad;
   WINDOW *menu;
   int pos{0};
-  //int pos1{0};
   int ch{0};
-  //int ch1{0};
+
+  char *choices[] = {"Open","Save","Save As", "Exit"};
+  ITEM **my_items;
+  int c;
+  MENU *my_menu;
+  int n_choices, i;
+  ITEM *cur_item;
+
+  /* Initialize items */
+  n_choices = ARRAY_SIZE(choices);
+  my_items = (ITEM **)calloc(n_choices + 1, sizeof(ITEM *));
+  for(i = 0; i < n_choices; ++i)
+    my_items[i] = new_item(choices[i], choices[i]);
+  my_items[n_choices] = (ITEM *)NULL;
+  item_opts_off(my_items[3], O_SELECTABLE);
+  item_opts_off(my_items[6], O_SELECTABLE);
+  /* Create menu */
+  my_menu = new_menu((ITEM **)my_items);
    if(argc != 2){//if there are not two arguments, print an error
      cout <<"Error, try again" << endl;
    }
@@ -30,18 +46,15 @@ int main(const int argc, const char * argv []){
      initscr();
      curs_set(0);//setting the cursor to not be visible
      getmaxyx(stdscr, row, col);
-    
-     x=row-5;
-     y=col-5;
+     x=row-3.5;
+     y=col-1;
      pad = newpad(x, y); //adding extra lines to ensure that the window is large enough for the file
      keypad(pad, TRUE);
+     printw("F1: Menu");
+     refresh();
      box(pad, 0, 0);
      wrefresh(pad);
-     if(myFile.is_open()){
-       while(getline(myFile, line)){
-	 //wprintw(pad, "%s\n", line.c_str()); 
-       }
-     }
+
      int tX,tY,bX,bY;
      tX = row/2 - x/2;
      tY = col/2 - y/2;
@@ -66,18 +79,12 @@ int main(const int argc, const char * argv []){
 	 */
        case KEY_F(1):
 	 int mRow, mCol;
-	 mRow = x/1.5;
-	 mCol = y/1.5;
+	 mRow = x/1.5;//initially divided by 2
+	 mCol = y/1.5;//initially divided by 2
 	 menu = newpad(mRow, mCol);
 	 box(menu, 0, 0);
 	 wrefresh(menu);
 	 keypad(menu, TRUE);
-
-	 /*tX = row/4;
-	 tY = col/4 - mCol/4;
-	 bX = mRow + tX;
-	 bY = mCol + tY;
-	 */
 
 	 tX = tX*2;
 	 tY = tY+4;
@@ -85,27 +92,6 @@ int main(const int argc, const char * argv []){
 	 bY = col + tY;
 
 	 prefresh(menu, 0, 0, tX, tY, bX, bY);
-	 /*while(true){
-	   ch1 = wgetch(menu);
-
-	  switch(ch1){//using switch statement to get the user's input
-
-	 case KEY_UP://check if up arrow is pressed
-	 if(pos1 >= 0){
-	   pos1--;
-	   }
-	   prefresh(menu, pos1, 0, 0, 0, row-1, col);//refresh pad
-	   break;
-
-	 case KEY_DOWN://check if down arrows are pressed
-	   pos1++;
-	   prefresh(menu, pos1, 0, 0, 0, row-1, col);//refresh pad
-	   break;	
-	   
-	 }//end while for F1
-	 //prefresh(menu, posMenu, 0, 0, 0, row/4 - 1, col);//refresh pad
-	
-	 }*/
        if(ch!=KEY_UP && ch!=KEY_DOWN && ch!=KEY_F(1)){//if key entered does not equal up or down, break out of the while loop
        break;
 	}
